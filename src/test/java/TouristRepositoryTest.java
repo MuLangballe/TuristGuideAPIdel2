8a4ca0ff822580f8df6b51fbeb4f13e6830696f6
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-02-28 09:44:58 +0100

    Added tests

---
+++ src/test/java/TouristRepositoryTest.java
@@ -0,0 +1,54 @@
+import com.example.turistguideapidel2.model.TouristAttraction;
+import com.example.turistguideapidel2.repository.TouristAttractionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TouristRepositoryTest {
+   private TouristAttractionRepository repository;
+
+    @BeforeEach
+   public void setUp() {
+        repository = new TouristAttractionRepository();
+    }
+
+    @Test
+    void attractionSize() {
+        List<TouristAttraction> attractions = repository.getListOfAttractions();
+        assertEquals(4, attractions.size());
+    }
+
+    @Test
+    void numberOfCities() {
+        List<String> expectedCities = List.of("København","Randers", "Næstved","Hareskovsby");
+        List<String> actualCities = repository.getCities();
+        assertEquals(expectedCities.size(),actualCities.size());
+    }
+
+    @Test
+    void numberOfTags() {
+        List<String> expectedTags = List.of("Børnevenlig ", "Kunst", "Gratis", "Natur", "Voksne");
+        List <String> acualTags = repository.getTags();
+        assertEquals(expectedTags.size(),acualTags.size());
+    }
+
+    @Test
+    void showTagsOnAttraction() {
+        String attractionName = "Havfrue";
+        List<String> expectedTags = List.of("Kunst","Gratis", "Børnevenlig");
+        List<String> attractionTagsList = repository.getAttractionTagsList(attractionName);
+
+        assertEquals(expectedTags.size(),attractionTagsList.size());
+
+    }
+
+
+
+
+
+}
--- src/test/java/com/example/turistguideapidel2/TuristGuideApIdel2ApplicationTests.java
+++ src/test/java/com/example/turistguideapidel2/TuristGuideApIdel2ApplicationTests.java
@@ -1,10 +1,16 @@
 package com.example.turistguideapidel2;
 
+import com.example.turistguideapidel2.controller.TouristAttractionController;
+import com.example.turistguideapidel2.service.TouristAttractionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class TuristGuideApIdel2ApplicationTests {
+
+    @Autowired
+    private TouristAttractionController controller;
+
 
     @Test
     void contextLoads() {
